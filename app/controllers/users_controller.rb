9536e649eeb3fb51d223460f2444d1df123641d0
vagrant <vagrant@localhost.localdomain>
2020-10-05 11:02:23 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @book = Book.new
+    @book_comment = BookComment.new
     @books = @user.books
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   get 'home/about' =>'homes#about'
-
-  resources :books, only: [:new, :create, :index, :show, :destroy,:edit,:update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :users, only: [:show,:edit, :update,:index]
+   resources :users, only: [:show,:edit, :update,:index]
+  resources :books, only: [:new, :create, :index, :show, :destroy,:edit,:update] do
+   resource :favorites, only: [:create, :destroy]
+   resource :book_comments, only: [:create, :destroy]
+  end
 end
 
